9a98518d92e0db4e2b530c39c96461330b2af826
bhangun <n90d1n9@gmail.com>
2026-09-18 17:58:03 +0700

    fix(spi): add readable() and writable() helper methods to FilesystemAccess

--- framework/wayang-spi/src/main/java/tech/kayys/wayang/spi/sandbox/FilesystemAccess.java
+++ framework/wayang-spi/src/main/java/tech/kayys/wayang/spi/sandbox/FilesystemAccess.java
@@ -4,5 +4,13 @@ public enum FilesystemAccess {
     NONE,
     READ,
     WRITE,
-    READ_WRITE
+    READ_WRITE;
+
+    public boolean readable() {
+        return this == READ || this == READ_WRITE;
+    }
+
+    public boolean writable() {
+        return this == WRITE || this == READ_WRITE;
+    }
 }
